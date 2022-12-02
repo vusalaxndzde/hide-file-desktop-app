e47ef81a8d09bfe580ecde021c03786b2040e60b
vusalaxndzde <vusal.axndzde@gmail.com>
2022-12-02 17:55:02 +0400

    hide changed

--- HideFile/src/main/java/com/company/hidefile/service/HideService.java
+++ HideFile/src/main/java/com/company/hidefile/service/HideService.java
@@ -1,19 +1,11 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.company.hidefile.service;
 
 import com.company.hidefile.bean.Config;
 import com.company.hidefile.util.FileUtil;
 
-/**
- *
- * @author Asus
- */
 public class HideService {
     
-    private static void hide(String imagePath, byte[] secretText, String fileExtension) throws Exception {
+    private void hide(String imagePath, byte[] secretText, String fileExtension) throws Exception {
         String container = imagePath;
         String steqoFile = FileUtil.newFileName(container, FileUtil.getExtension(container));
         FileUtil.writeBytes(steqoFile, FileUtil.readBytes(container));
@@ -22,13 +14,13 @@ public class HideService {
         FileUtil.appendBytes(steqoFile, (Config.getExtensionKey() + fileExtension).getBytes());
     }
 
-    public static void hideFile(String imagePath, String filePath) throws Exception {
+    public void hideFile(String imagePath, String filePath) throws Exception {
         byte[] secretText = FileUtil.readBytes(filePath);
         String fileExtension = FileUtil.getExtension(filePath);
         hide(imagePath, secretText, fileExtension);
     }
 
-    public static void hideText(String imagePath, String text) throws Exception {
+    public void hideText(String imagePath, String text) throws Exception {
         byte[] secretText = text.getBytes();
         String fileExtension = "txt";
         hide(imagePath, secretText, fileExtension);
